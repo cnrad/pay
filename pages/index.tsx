5e60f774f108ffe571e07f30b0d3acc3ca3547b1
cnrad <83192247+cnrad@users.noreply.github.com>
2021-09-08 21:31:49 -0400

    try discord meta embed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,57 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Head from 'next/head';
 
 export default function Home() {
+
+    //thanks alistair vvv
+    addEventListener('fetch', (event: any) => {
+        event.respondWith(handleRequest(event.request));
+    });
+
+    const document = `
+        <!DOCTYPE html>
+        <html>
+                <head>
+                        <title>pay.cnrad.dev</title>
+                        <meta name="theme-color" content="#4284f5" />
+                        <meta name="og:description" content="Pay Conrad via PayPal" />
+                        <meta name="og:title" content="pay.cnrad.dev" />
+                        <meta name="og:image" content="/paypal.png" />
+                </head>
+        </html>
+    `;
+
+    async function handleRequest(request: any) {
+        const userAgent = request.headers.get("User-Agent") ?? "";
+        const isDiscord = userAgent.toLowerCase().includes("discord");
+      
+        if (isDiscord) {
+            return new Response(document, {
+                headers: {
+                  "content-type": "text/html;charset=UTF-8",
+                },
+            });
+        }
+      
+      
+        const reqURL = new URL(request.url);
+        return Response.redirect("https://paypal.me/cnradd/" + reqURL.pathname, 301);
+    }
+    //thanks alistair ^^^
+
     return(
-        <Page>
-            <Text>Redirecting shortly...</Text>
-        </Page>
+        <>
+            <Head>
+		        <title>pay.cnrad.dev</title>
+		        <meta name="theme-color" content="#4284f5" />
+		        <meta name="og:description" content="Pay Conrad via PayPal" />
+		        <meta name="og:title" content="pay.cnrad.dev" />
+		        <meta name="og:image" content="/paypal.png" />
+            </Head>
+            <Page>
+                <Text>Redirecting shortly...</Text>
+            </Page>
+        </>
     )
 }
 
